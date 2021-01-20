9c81e59ea5b6183d2fdd2a6e796cfe65011297c9
Taylor Darneille <tmdarneille@gmail.com>
2021-01-20 14:39:16 -0800

    sign up route complete (for now)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 
 router.get('/signup', (req, res)=>{
     res.render('auth/signup.ejs')
 })
 
 router.post('/signup', (req, res)=>{
-    res.send(req.body)
+    // find or create a new user!
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        },
+        defaults: {
+            name: req.body.name,
+            password: req.body.password
+        }
+    })
+    .then(([user, wasCreated])=>{
+        if(wasCreated){
+            res.send(`Created a new user profile for ${user.email}`)
+        } else {
+            res.send('Email already exists! try loggin in?')
+        }
+    })
 })
 
 router.get('/login', (req, res)=>{
