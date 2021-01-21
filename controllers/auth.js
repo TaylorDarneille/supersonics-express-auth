b781fd5f61c556287fd0159a44918893742dade5
Taylor Darneille <tmdarneille@gmail.com>
2021-01-21 12:03:33 -0800

    finished setting up passport

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const passport = require('../config/ppConfig.js')
 
 router.get('/signup', (req, res)=>{
     res.render('auth/signup.ejs')
@@ -19,9 +20,11 @@ router.post('/signup', (req, res)=>{
     })
     .then(([user, wasCreated])=>{
         if(wasCreated){
+            passport.authenticate('local', {successRedirect: '/'})(req, res) // IIFE
             res.send(`Created a new user profile for ${user.email}`)
         } else {
-            res.send('Email already exists! try loggin in?')
+            console.log('An account associate with that email address already exists! Did you mean to log in?')
+            res.redirect('/auth/login')
         }
     })
 })
@@ -30,28 +33,14 @@ router.get('/login', (req, res)=>{
     res.render('auth/login.ejs')
 })
 
-router.post('/login', (req, res)=>{
-    db.user.findOne({
-        where: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    })
-    .then(foundUser=>{
-        if(foundUser){
-            res.send(`Logged in the following user: ${foundUser.name}`)
-        } else {
-            res.send('don\'t know her - try signing up?')
-        }
-    })
-    .catch(err=>{
-        console.log(err)
-        res.send('There was an error loggin in. Check the console?')
-    })
-})
+router.post('/login', passport.authenticate('local', {
+    failureRedirect: '/auth/login',
+    successRedirect: '/'
+}))
 
 router.get('/logout', (req, res)=>{
-    res.send('you have been logged out')
+    req.logout()
+    res.redirect('/')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config() // configure environment variables
 const express = require('express')
 const app = express()
+const session = require('express-session')
+const passport = require('./config/ppConfig.js')
 
 // set the view enging to ejs
 app.set('view engine', 'ejs')
@@ -10,11 +12,27 @@ app.use(require('express-ejs-layouts'))
 //body parser middleware allows us to receive form data in req.body
 app.use(express.urlencoded({extended: false}))
 
+// session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+// passport middlware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // controller middleware
 app.use('/auth', require('./controllers/auth.js'))
 
 app.get('/', (req, res)=>{
-    res.render('home')
+    if(req.user) {
+        res.send(`req.user: ${req.user.name}`)
+    } else {
+        res.send('no user is currently logged in')
+    }
+    // res.render('home')
 })
 
 app.get('/profile', (req, res)=>{
